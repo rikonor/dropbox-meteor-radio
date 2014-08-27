26996a83c4f992239398fd98c1b579ff39538dde
Or Rikon <rikonor@gmail.com>
2014-08-27 20:46:21 +0300

    sharing of links between server and client

--- radio-meteor.js
+++ radio-meteor.js
@@ -1,23 +1,95 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+// Utils
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+var DropboxUtils = {
+  createClient: function() {
+    var client = new Dropbox.Client({
+      key: "hmszotb9dwz2cpz",
+      secret: "0oktwgqm6kjpa1o",
+      sandbox: false,
+      token: "95pyx-gDahwAAAAAAAAFnN97dL-iHLkUWzH8AzgequGcrUcf5L7HttopEyNrpcnx"
+    });
+    return client;
+  },
+  getRootContent: function(client, callback) {
+    client.readdir("/", callback);
+  },
+  mediaLinks: function(client, paths, callback) {
+    async.map(paths, function(path, callback) {
+      client.makeUrl(path, {download: true}, function(err, data) {
+        if (err) { return callback(err); }
+        callback(null, data.url);
+      });
+    }, callback);
+  }
+};
+
+// Client
+
+if (Meteor.isClient) {
+  var songsList = [];
+
+  var SongsUtil = {
+    getAllSongs: function(callback) {
+        Meteor.call('getAllSongs', function(err, data) {
+        if (err) { return callback(err); }
+        callback(null, data);
+      });  
+    },
+    getRandomSong: function(songsList) {
+      var i = Math.floor(Math.random() * songsList.length);
+      return songsList[i];
+    }
+  };
+
+  setTimeout(function() {
+    SongsUtil.getAllSongs(function(err, data) {
+      if (err) { return console.log(err); }
+      if (!data || data.length == 0) { return console.log("No songs found."); }
+      songsList = data;
+    });
+  }, 2000);
+
+  // Session.setDefault("currentSong", songsList[0] || undefined)
+
+  Template.player.helpers({
+    currentSong: function () {
+      return Session.get("currentSong") || "N/A";
     }
   });
 
-  Template.hello.events({
+  Template.player.events({
     'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+      Session.set("currentSong", SongsUtil.getRandomSong(songsList));
     }
   });
 }
 
+// Server
+
 if (Meteor.isServer) {
+  var songsList = [];
+
+  Meteor.methods({
+    getAllSongs: function() {
+      return songsList;
+    }
+  });
+
   Meteor.startup(function () {
-    // code to run on server at startup
+    var client = DropboxUtils.createClient();
+    DropboxUtils.getRootContent(client, function(err, data) {
+      if (err) { return console.log(err); }
+      if (!data || data.length == 0) {
+        return console.log("Root folder is empty.");
+      }
+      DropboxUtils.mediaLinks(client, data, function(err, data) {
+        if (err) { return console.log(err); }
+        if (!data || data.length == 0) {
+          return console.log("Failed to get media links.");
+        }
+        songsList = data;
+        console.log(songsList);
+      });
+    });
   });
 }
